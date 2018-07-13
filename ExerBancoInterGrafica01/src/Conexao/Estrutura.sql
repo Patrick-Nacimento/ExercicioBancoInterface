8cffc460f238c854d08563f578eb2b87cf021146
Patrick-Nacimento <38558599+Patrick-Nacimento@users.noreply.github.com>
2018-07-13 11:26:37 -0300

    Iniciando

---
+++ ExerBancoInterGrafica01/src/Conexao/Estrutura.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS ExerBancoInterGrafica01;
+CREATE DATABASE IF NOT EXISTS ExerBancoInterGrafica01;
+
+USE ExerBancoInterGrafica01;
+
+CREATE TABLE alunos(
+    id                  INT AUTO_INCREMENT PRIMARY KEY,
+    nome                VARCHAR(100) NOT NULL,
+    codigo_matricula    VARCHAR(10),
+    nota1               FLOAT,
+    nota2               FLOAT,
+    nota3               FLOAT,
+    frequencia          TINYINT
+
+);
---
+++ ExerBancoInterGrafica01/src/Conexao/Estrutura02.sql
@@ -0,0 +1,2 @@
+USE ExerBancoInterGrafica01;
+SELECT nome, codigo_matricula, nota1, nota2, nota3, frequencia  FROM alunos;
